f7ddfe711490603bd78895825631256e0449817d
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2012-04-18 20:25:12 +0200

    Adjusted the paths to the sql_access-files of the installation-files

--- code/installation/price_classes.php
+++ code/installation/price_classes.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
 
-require_once '../include/PriceClassManager.php';
-require_once '../include/GroupManager.php';
+require_once '../include/sql_access/PriceClassManager.php';
+require_once '../include/sql_access/GroupManager.php';
 require_once 'installation_constants.php';
 
 if(!_AEXEC){
