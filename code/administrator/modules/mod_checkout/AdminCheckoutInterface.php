b8e523a4fbc77b2b30fae2ccab62e0891a85bdd6
Pascal Ernst <pacal.cc.ernst@googlemail.com>
2012-05-19 14:29:42 +0200

    Heavy refactoring of mod_checkout to use the new Object-orientated codestyle. Additional Method-Extracts.

---
+++ code/administrator/modules/mod_checkout/AdminCheckoutInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+class AdminCheckoutInterface extends AdminInterface {
+	
+	////////////////////////////////////////////////////////////////////////////////
+	//Attributes
+	protected $folderPath;
+	
+	////////////////////////////////////////////////////////////////////////////////
+	//Constructor
+	public function __construct() {
+		
+		parent::__construct();
+		
+		$this->folderPath = PATH_SMARTY_ADMIN_MOD . '/mod_checkout/';
+		$this->parentPath = $this->folderPath . 'mod_checkout_header.tpl';
+		$this->smarty->assign('checkoutParent', $this->parentPath);
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////
+	//Methods
+	/**
+	 * Shows a "card-is-locked"-Dialog
+	 */
+	public function CardLocked() {
+		$this->smarty->display(PATH_SMARTY_CHECKOUT.'/checkout_locked.tpl');
+	}
+	
+	public function Checkout($mealnames) {
+		$this->smarty->assign('meal_names', $mealnames);
+		$this->smarty->display($this->folderPath . 'checkout.tpl');
+	}
+	
+	public function CardId() {
+		$this->smarty->display('administrator/modules/mod_checkout/form.tpl');
+	}
+	
+}
+
+?>
--- code/administrator/modules/mod_checkout/checkout.php
+++ code/administrator/modules/mod_checkout/checkout.php
@@ -2,68 +2,18 @@
 
     //no direct access
     defined('_AEXEC') or die("Access denied");
-    global $smarty;
 
-	require_once PATH_ACCESS . '/dbconnect.php';
-	require_once PATH_ACCESS . '/CardManager.php';
-	require_once PATH_ACCESS . '/UserManager.php';
-	require_once PATH_ACCESS . '/OrderManager.php';
-    require "checkout_constants.php"; 
-
-    $cardManager = new CardManager();
-    $userManager = new UserManager();
-    $orderManager = new OrderManager();
-    $smarty->assign('checkoutParent', PATH_SMARTY_ADMIN_MOD.'/mod_checkout/mod_checkout_header.tpl');
+    require_once 'AdminCheckoutProcessing.php';
+    require_once 'AdminCheckoutInterface.php';
     
-	if ('POST' == $_SERVER['REQUEST_METHOD']) {
-	   if (!isset($_POST['card_ID'])) {
-		  die(INVALID_FORM);
-		}  //save values and check for empty fields
-		if (($card_id = trim($_POST['card_ID'])) == '' OR !$cardManager->valid_card_ID($card_id)) {
-	        die(EMPTY_FORM);
-	   	}
-	   	try {
-	   		$uid = $cardManager->getUserID($card_id);
-	   		if ($userManager->checkAccount($uid)) {
-	   			$smarty->display(PATH_SMARTY_CHECKOUT.'/checkout_locked.tpl');
-	   			exit();
-	   		}
-	   	} catch (Exception $e) {
-	   		die_error(ERR_GET_USER_BY_CARD.' Error:'.$e->getMessage());die();
-	   	}
-	   
-	   	
-	   	$date = date("Y-m-d");
-	   	$orders = array();
-	   	try {
-	   		$orders = $orderManager->getAllOrdersOfUserAtDate($uid, $date);
-	   	} catch (MySQLVoidDataException $e) {
-	   		$smarty->display(PATH_SMARTY_CHECKOUT.'/checkout_no_orders.tpl');
-	   		exit();
-	   	} catch(Exception $e) {
-	   		die('An Error ocurred:'.$e->getMessage());
-	   	}
-	   	$meal_names = array();
-		for ($i = 0; $i < count($orders); $i++) {
-            $row = $orders[$i];
-            $meal_name = $mealManager->getEntryData($row['MID'], 'name');
-            // Abfrage des feldes 'name' aus der Tabelle 'meals' mit der ID '$row['MID'], anschlie�end Ausgabe des Namens
-            if(!$meal_name) {
-                die(MEAL_NOT_FOUND);
-            }
-            if(!$orderManager->OrderFetched($row['ID'])) {
-                $meal_names[] = $meal_name['name'];
-                $orderManager->setOrderFetched($row['ID']);    
-            } 
-            else {
-                $meal_names[] = $meal_name['name'].'; '.ORDER_ALREADY_FETCHED;
-            }
-		}
-        $smarty->assign('meal_names', $meal_names);
-        $smarty->display('administrator/modules/mod_checkout/checkout.tpl');	
+    $checkoutProcessing = new AdminCheckoutProcessing();
+    $checkoutInterface = new AdminCheckoutInterface();
+    
+	if ('POST' == $_SERVER['REQUEST_METHOD'] && isset($_POST['card_ID'])) {
+		$checkoutProcessing->Checkout($_POST['card_ID']);	
     }
 	else{
-	   $smarty->display('administrator/modules/mod_checkout/form.tpl');
+		$checkoutInterface->CardId();
     }
       
 ?>
