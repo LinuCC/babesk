8c2531a4625a1f831b6ecbb91d911b2b03b8cbde
inchem <mirek@hancl.de>
2012-06-30 06:54:35 +0200

    testing branch for Schbas (Schulbuchausleihsystem)and Fits (Führerschein IT-Systeme). Interface and Processing code simply copied, must be replaced, sure :-)

---
+++ code/administrator/headmod_Fits/Fits.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once PATH_INCLUDE . '/HeadModule.php';
+
+/**
+ * class for Interface administrator
+ * @author Mirek Hancl
+ *
+ */
+class Fits extends HeadModule {
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Attributes
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Constructor
+	public function __construct($name, $display_name) {
+		parent::__construct($name, $display_name);
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Methods
+	public function execute() {
+		//function not needed, javascript is doing everything
+	}
+	
+	public function executeModule($mod_name, $dataContainer) {
+		parent::executeModule($mod_name, $dataContainer);
+	}
+}
+?>
