40bb7f74319a004b5fe96b64eb21268a05b81408
infchem <mirek@hancl.de>
2012-10-04 16:53:04 +0200

    reunion party

--- code/include/sql_access/databaseDistributor.php
+++ code/include/sql_access/databaseDistributor.php
@@ -6,12 +6,7 @@
  * @todo: Remove this file and replace the occurences with DBConnect.php and the Class DBConnect
  */
 
-require_once dirname(__FILE__) . '/DBConnect.php.bak';
-
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'babesk';
+require_once dirname(__FILE__) . '/DBConnect.php';
 
 // $dbObject = new DBConnect($host, $username, $password, $database);
 $dbObject = new DBConnect();
