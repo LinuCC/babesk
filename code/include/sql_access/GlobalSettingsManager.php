77986e91bf05d75a9acc6bb02d494871b995bacb
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2012-10-29 23:39:59 +0100

    Added "GlobalSettings"-class; Added some functions to GlobalSettingsManager for abstraction; refactored some functions.

--- code/include/sql_access/GlobalSettingsManager.php
+++ code/include/sql_access/GlobalSettingsManager.php
@@ -1,11 +1,21 @@
 <?php
 require_once PATH_ACCESS . '/TableManager.php';
 
+/** This class contains the names of the globalSettings in the database
+ * It acts as an enum
+ */
+abstract class GlobalSettings {
+	const HELPTEXT = 'helptext';
+	const RELIGION = 'religion';
+	const SPECIAL_COURSE = 'special_course';
+	const FOREIGN_LANGUAGE = 'foreign_language';
+}
+
 class GlobalSettingsManager extends TableManager {
 	function __construct() {
 		parent::__construct('global_settings');
 	}
-	
+
 	/**
 	 * returns the helptext found in the MySQL-table global_settings
 	 * @throws MySQLVoidDataException
@@ -13,12 +23,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @return string
 	 */
 	function getHelpText() {
-		$entry_arr = $this->searchEntry('name="helptext"');
-		if (!isset($entry_arr['value']) || !$entry_arr['value'])
-			throw new MySQLVoidDataException('helptext is void!');
-		return $entry_arr['value'];
+		return $this->valueGet (GlobalSettings::HELPTEXT);
 	}
-	
+
 	/**
 	 * Changes the HelpText
 	 * @param string $str the text will be changed to this string
@@ -26,14 +33,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @throws some other things if somethings gone wrong
 	 */
 	function changeHelpText($str) {
-		$entry_arr = $this->searchEntry('name="helptext"');
-		if (!isset($entry_arr) || !count($entry_arr)) {
-			throw new MySQLVoidDataException('searchEntry returned void helparray');
-		}
-		$help_id = $entry_arr['id'];
-		$this->alterEntry($help_id, 'value', $str);
+		$this->valueSet (GlobalSettings::HELPTEXT, $str);
 	}
-	
+
 	/**
 	 * returns the religions found in the MySQL-table global_settings
 	 * @throws MySQLVoidDataException
@@ -41,12 +43,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @return string
 	 */
 	function getReligion() {
-		$entry_arr = $this->searchEntry('name="religion"');
-		if (!isset($entry_arr['value']) || !$entry_arr['value'])
-			throw new MySQLVoidDataException('religion is void!');
-		return $entry_arr['value'];
+		return $this->valueGet (GlobalSettings::RELIGION);
 	}
-	
+
 	/**
 	 * Sets the Religions
 	 * @param string $str the text will be changed to this string
@@ -54,14 +53,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @throws some other things if somethings gone wrong
 	 */
 	function setReligion($str) {
-		$entry_arr = $this->searchEntry('name="religion"');
-		if (!isset($entry_arr) || !count($entry_arr)) {
-			throw new MySQLVoidDataException('searchEntry returned void helparray');
-		}
-		$religion_id = $entry_arr['id'];
-		$this->alterEntry($religion_id, 'value', $str);
+		$this->valueSet (GlobalSettings::RELIGION, $str);
 	}
-	
+
 	/**
 	 * returns the courses found in the MySQL-table global_settings
 	 * @throws MySQLVoidDataException
@@ -69,12 +63,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @return string
 	 */
 	function getCourse() {
-		$entry_arr = $this->searchEntry('name="special_course"');
-		if (!isset($entry_arr['value']) || !$entry_arr['value'])
-			throw new MySQLVoidDataException('special_course is void!');
-		return $entry_arr['value'];
+		return $this->valueGet (GlobalSettings::SPECIAL_COURSE);
 	}
-	
+
 	/**
 	 * Sets the Courses
 	 * @param string $str the text will be changed to this string
@@ -82,15 +73,10 @@ class GlobalSettingsManager extends TableManager {
 	 * @throws some other things if somethings gone wrong
 	 */
 	function setCourse($str) {
-		$entry_arr = $this->searchEntry('name="special_course"');
-		if (!isset($entry_arr) || !count($entry_arr)) {
-			throw new MySQLVoidDataException('searchEntry returned void helparray');
-		}
-		$course_id = $entry_arr['id'];
-		$this->alterEntry($course_id, 'value', $str);
+		$this->valueSet (GlobalSettings::SPECIAL_COURSE, $str);
 	}
-	
-	
+
+
 	/**
 	 * returns the foreign languages found in the MySQL-table global_settings
 	 * @throws MySQLVoidDataException
@@ -98,12 +84,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @return string
 	 */
 	function getForeignLanguages() {
-		$entry_arr = $this->searchEntry('name="foreign_language"');
-		if (!isset($entry_arr['value']) || !$entry_arr['value'])
-			throw new MySQLVoidDataException('foreign_lang is void!');
-		return $entry_arr['value'];
+		return $this->valueGet (GlobalSettings::FOREIGN_LANGUAGE);
 	}
-	
+
 	/**
 	 * Sets the foreign languages
 	 * @param string $str the text will be changed to this string
@@ -111,12 +94,7 @@ class GlobalSettingsManager extends TableManager {
 	 * @throws some other things if somethings gone wrong
 	 */
 	function setForeignLanguages($str) {
-		$entry_arr = $this->searchEntry('name="foreign_language"');
-		if (!isset($entry_arr) || !count($entry_arr)) {
-			throw new MySQLVoidDataException('searchEntry returned void helparray');
-		}
-		$foreignLanguage_id = $entry_arr['id'];
-		$this->alterEntry($foreignLanguage_id, 'value', $str);
+		$this->valueSet (GlobalSettings::FOREIGN_LANGUAGE, $str);
 	}
 
 	/**
@@ -126,12 +104,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @return string
 	 */
 	function getSpecialCourses() {
-		$entry_arr = $this->searchEntry('name="special_course"');
-		if (!isset($entry_arr['value']) || !$entry_arr['value'])
-			throw new MySQLVoidDataException('special_course is void!');
-		return $entry_arr['value'];
+		return $this->valueGet (GlobalSettings::SPECIAL_COURSE);
 	}
-	
+
 	/**
 	 * Sets the special courses
 	 * @param string $str the text will be changed to this string
@@ -139,14 +114,9 @@ class GlobalSettingsManager extends TableManager {
 	 * @throws some other things if somethings gone wrong
 	 */
 	function setSpecialCourses($str) {
-		$entry_arr = $this->searchEntry('name="special_course"');
-		if (!isset($entry_arr) || !count($entry_arr)) {
-			throw new MySQLVoidDataException('searchEntry returned void helparray');
-		}
-		$specialCourse_id = $entry_arr['id'];
-		$this->alterEntry($specialCourse_id, 'value', $str);
-	}	
-	
+		$this->valueSet (GlobalSettings::SPECIAL_COURSE, $str);
+	}
+
 	/**
 	 * Returns both of the infotexts of the table global_settings
 	 * Enter description here ...
@@ -156,16 +126,16 @@ class GlobalSettingsManager extends TableManager {
 		$it_arr = array();
 		$it1 = $this->searchEntry('name="menu_text1"');
 		$it2 = $this->searchEntry('name="menu_text2"');
-		
+
 		$it_arr[0] = $it1['value'];
 		$it_arr[1] = $it2['value'];
-		
+
 		if (!$it_arr[0] || !$it_arr[1]) {
 			throw new MySQLVoidDataException('MySQL returned a void element!');
 		}
 		return $it_arr;
 	}
-	
+
 	/**
 	 * Changes infotext1 and infotext2 based on the arguments given
 	 */
@@ -173,7 +143,7 @@ class GlobalSettingsManager extends TableManager {
 		$this->alterEntry($this->getIDByValue('name', 'menu_text1'), 'value', $infotext1);
 		$this->alterEntry($this->getIDByValue('name', 'menu_text2'), 'value', $infotext2);
 	}
-	
+
 	/**
 	 * changes the Last Order Time
 	 * @param unknown_type $time
@@ -181,7 +151,7 @@ class GlobalSettingsManager extends TableManager {
 	function setLastOrderTime($time) {
 		$this->alterEntry($this->getIDByValue('name', 'last_order_time'), 'value', $time);
 	}
-	
+
 	/**
 	 * returns the value of soli_price
 	 * @throws UnexpectedValueException when soli_price is NULL
@@ -195,7 +165,7 @@ class GlobalSettingsManager extends TableManager {
 			throw new UnexpectedValueException('soli_price has no value!');
 		return $soli_price;
 	}
-	
+
 	/**
 	 * returns the key for Fits module
 	 * @throws UnexpectedValueException when fits is NULL
@@ -209,8 +179,7 @@ class GlobalSettingsManager extends TableManager {
 			throw new UnexpectedValueException('fits_key has no value!');
 		return $fits_key;
 	}
-	
-	
+
 	/**
 	 * changes the Fits key
 	 * @param unknown_type $key
@@ -218,8 +187,7 @@ class GlobalSettingsManager extends TableManager {
 	function setFitsKey($key) {
 		$this->alterEntry($this->getIDByValue('name', 'fits_key'), 'value', $key);
 	}
-	
-	
+
 	/**
 	 * returns the schoolyear for Fits module
 	 * @throws UnexpectedValueException when schoolyear is NULL
@@ -234,8 +202,6 @@ class GlobalSettingsManager extends TableManager {
 		return $fits_year;
 	}
 
-	
-	
 	/**
 	 * changes the Fits year
 	 * @param unknown_type $year
@@ -243,7 +209,7 @@ class GlobalSettingsManager extends TableManager {
 	function setFitsYear($year) {
 		$this->alterEntry($this->getIDByValue('name', 'fits_year'), 'value', $year);
 	}
-	
+
 	/**
 	 * returns the class for Fits module
 	 * @throws UnexpectedValueException when class is NULL
@@ -256,17 +222,16 @@ class GlobalSettingsManager extends TableManager {
 		if($fits_class === NULL)
 			throw new UnexpectedValueException('fits_year has no value!');
 		return $fits_class;
-	}	
-	
-	
+	}
+
 	/**
 	 * changes the Fits class
 	 * @param unknown_type $class
 	 */
 	function setFitsClass($class) {
 		$this->alterEntry($this->getIDByValue('name', 'fits_class'), 'value', $class);
-	}	
-	
+	}
+
 	/**
 	 * returns the search method for Fits module
 	 * @throws UnexpectedValueException when search method is NULL
@@ -280,17 +245,15 @@ class GlobalSettingsManager extends TableManager {
 			throw new UnexpectedValueException('fits_all_classes has no value!');
 		return $fits_class;
 	}
-	
-	
+
 	/**
 	 * changes the Fits search methof
 	 * @param unknown_type $flag
 	 */
 	function setFitsAllClasses($flag) {
 		$this->alterEntry($this->getIDByValue('name', 'fits_all_classes'), 'value', $flag);
-	}	
-	
-	
+	}
+
 	/**
 	 * Changes the value of "soli_price" to the given value
 	 * @throws something if something has gone wrong
@@ -299,7 +262,7 @@ class GlobalSettingsManager extends TableManager {
 		$pid = parent::searchEntry("name = 'soli_price'");
 		parent::alterEntry($pid['id'], 'value', $value);
 	}
-	
+
 	/**
 	* returns the value of last_order_time
 	* @throws UnexpectedValueException when last_order_time is NULL
@@ -312,6 +275,34 @@ class GlobalSettingsManager extends TableManager {
 		if($lastOrderTime === NULL)
 		throw new UnexpectedValueException('last_order_time has no value!');
 		return $lastOrderTime;
+	}
+
+	/** Sets the Global Setting that has the name $name to the value $value
+	 * If no entry with this name is found, the entry will be created in the
+	 * table with the value $value
+	 */
+	public function valueSet ($name, $value) {
+		try {
+			$id = $this->getIDByValue ('name', $name);
+		} catch (MySQLVoidDataException $e) {
+			//nothing found, create new entry
+			$this->addEntry ('name', $name, 'value', $value);
+			return;
+		}
+		$this->alterEntry ($id, 'value', $value);
+	}
+
+	/** Returns the value of the GlobalSettings-entry with the name $name
+	 * @throws MySQLVoidDataException if no entry with the name $name was found
+	 */
+	public function valueGet ($name) {
+		try {
+			$id = $this->getIDByValue ('name', $name);
+			$value = $this->getEntryValue ($id, 'value');
+		} catch (MySQLVoidDataException $e) {
+			throw new MySQLVoidDataException (sprintf('Global Setting not found: %s', $name));
+		}
+		return $value;
 	}
 }
 ?>
