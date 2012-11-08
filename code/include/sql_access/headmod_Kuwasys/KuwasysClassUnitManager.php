23be387015b8fcce8314ee0c75d2e39be3fe775d
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2012-11-08 14:30:53 +0100

    Refactored the new Code In KuwasysClassUnitManager, deleted the "weekday"-entry of the Table Classes

--- code/include/sql_access/headmod_Kuwasys/KuwasysClassUnitManager.php
+++ code/include/sql_access/headmod_Kuwasys/KuwasysClassUnitManager.php
@@ -3,7 +3,7 @@
 require_once PATH_ACCESS . '/TableManager.php';
 
 /** This Class acts like a struct, it allows to abstract from the MySQL-table Variable names
- *
+ * @author Pascal Ernst <pascal.cc.ernst@gmail.com>
  */
 abstract class KuwasysClassUnit {
 	const id = 'ID';
@@ -12,69 +12,68 @@ abstract class KuwasysClassUnit {
 }
 
 /** Manages the Access to the Table kuwasysClassUnitManager of the Babesk-MySQL-Database
- *
  *@author Pascal Ernst <pascal.cc.ernst@gmail.com>
  */
 class KuwasysClassUnitManager extends TableManager {
 
-		////////////////////////////////////////////////////////////////////////////////
-		//Attributes
-		////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	//Constructor
+	////////////////////////////////////////////////////////////////////////////
+	public function __construct($interface = NULL) {
+		parent::__construct('kuwasysClassUnit');
+	}
 
-		////////////////////////////////////////////////////////////////////////////////
-		//Constructor
-		////////////////////////////////////////////////////////////////////////////////
-		public function __construct($interface = NULL) {
-			parent::__construct('kuwasysClassUnit');
-		}
+	////////////////////////////////////////////////////////////////////////////
+	//Getters and Setters
+	////////////////////////////////////////////////////////////////////////////
 
-		////////////////////////////////////////////////////////////////////////////////
-		//Getters and Setters
-		////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	//Methods
+	////////////////////////////////////////////////////////////////////////////
+	/** Returns a Class-Unit with the given ID
+	 * @param $id The ID of the ClassUnit to search for
+	 * @return An Array with the Data of the ClassUnit
+	 */
+	public function unitGet ($id) {
+		return $this->searchEntry (sprintf('%s = %s',KuwasysClassUnit::id, $id));
+	}
 
-		////////////////////////////////////////////////////////////////////////////////
-		//Methods
-		////////////////////////////////////////////////////////////////////////////////
-		/** Returns a Class-Unit with the given ID
-		 * @param $id The ID of the ClassUnit to search for
-		 * @return An Array with the Data of the ClassUnit
-		 */
-		public function unitGet ($id) {
-			return $this->searchEntry (sprintf('%s = %s',KuwasysClassUnit::id, $id));
-		}
+	/** Returns all ClassUnits in the Table
+	 * @return An array of an array, The Units and their Data
+	 */
+	public function unitGetAll () {
+		return $this->getTableData ();
+	}
 
-		/** Returns all ClassUnits in the Table
-		 * @return An array of an array, The Units and their Data
-		 */
-		public function unitGetAll () {
-			return $this->getTableData ();
-		}
+	/** Returns the first found ClassUnit with the given name
+	 * @param $name The Name of the Unit to search for
+	 * @return An Array with the Data of the ClassUnit
+	 */
+	public function unitGetByName ($name) {
+		return $this->searchEntry (sprintf('%s = %s',KuwasysClassUnit::name, $id));
+	}
 
-		/** Returns the first found ClassUnit with the given name
-		 * @param $name The Name of the Unit to search for
-		 * @return An Array with the Data of the ClassUnit
-		 */
-		public function unitGetByName ($name) {
-			return $this->searchEntry (sprintf('%s = %s',KuwasysClassUnit::name, $id));
-		}
+	/** Adds an ClassUnit-Entry to the table
+	 * @param $name the Name of the ClassUnit
+	 * @param $translatedName the translatedName (shown to the User) of the ClassUnit
+	 */
+	public function unitAdd ($name, $translatedName) {
+		$this->addEntry ($id, $name, $translatedName);
+	}
 
-		/** Adds an ClassUnit-Entry to the table
-		 * @param $name the Name of the ClassUnit
-		 * @param $translatedName the translatedName (shown to the User) of the ClassUnit
-		 */
-		public function unitAdd ($name, $translatedName) {
-			$this->addEntry ($id, $name, $translatedName);
-		}
+	/** Deletes an ClassUnit from the Table
+	 * @param $id The ID of the ClassUnit to delete
+	 */
+	public function unitDelete ($id) {
+		$this->delEntry ($id);
+	}
+	////////////////////////////////////////////////////////////////////////////
+	//Implementations
+	////////////////////////////////////////////////////////////////////////////
 
-		/** Deletes an ClassUnit from the Table
-		 * @param $id The ID of the ClassUnit to delete
-		 */
-		public function unitDelete ($id) {
-			$this->delEntry ($id);
-		}
-		////////////////////////////////////////////////////////////////////////////////
-		//Implementations
-		////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	//Attributes
+	////////////////////////////////////////////////////////////////////////////
 
 }
 
