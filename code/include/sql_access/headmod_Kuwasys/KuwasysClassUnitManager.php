dd5586b483c87cf16b241910cd4869c939e23a71
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2012-11-08 14:26:34 +0100

    Added The Class and file KuwasysClassUnitManager that manages the Access to the new MySQL-table kuwasysClassUnit

---
+++ code/include/sql_access/headmod_Kuwasys/KuwasysClassUnitManager.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once PATH_ACCESS . '/TableManager.php';
+
+/** This Class acts like a struct, it allows to abstract from the MySQL-table Variable names
+ *
+ */
+abstract class KuwasysClassUnit {
+	const id = 'ID';
+	const name = 'name';
+	const translatedName = 'translatedName';
+}
+
+/** Manages the Access to the Table kuwasysClassUnitManager of the Babesk-MySQL-Database
+ *
+ *@author Pascal Ernst <pascal.cc.ernst@gmail.com>
+ */
+class KuwasysClassUnitManager extends TableManager {
+
+		////////////////////////////////////////////////////////////////////////////////
+		//Attributes
+		////////////////////////////////////////////////////////////////////////////////
+
+		////////////////////////////////////////////////////////////////////////////////
+		//Constructor
+		////////////////////////////////////////////////////////////////////////////////
+		public function __construct($interface = NULL) {
+			parent::__construct('kuwasysClassUnit');
+		}
+
+		////////////////////////////////////////////////////////////////////////////////
+		//Getters and Setters
+		////////////////////////////////////////////////////////////////////////////////
+
+		////////////////////////////////////////////////////////////////////////////////
+		//Methods
+		////////////////////////////////////////////////////////////////////////////////
+		/** Returns a Class-Unit with the given ID
+		 * @param $id The ID of the ClassUnit to search for
+		 * @return An Array with the Data of the ClassUnit
+		 */
+		public function unitGet ($id) {
+			return $this->searchEntry (sprintf('%s = %s',KuwasysClassUnit::id, $id));
+		}
+
+		/** Returns all ClassUnits in the Table
+		 * @return An array of an array, The Units and their Data
+		 */
+		public function unitGetAll () {
+			return $this->getTableData ();
+		}
+
+		/** Returns the first found ClassUnit with the given name
+		 * @param $name The Name of the Unit to search for
+		 * @return An Array with the Data of the ClassUnit
+		 */
+		public function unitGetByName ($name) {
+			return $this->searchEntry (sprintf('%s = %s',KuwasysClassUnit::name, $id));
+		}
+
+		/** Adds an ClassUnit-Entry to the table
+		 * @param $name the Name of the ClassUnit
+		 * @param $translatedName the translatedName (shown to the User) of the ClassUnit
+		 */
+		public function unitAdd ($name, $translatedName) {
+			$this->addEntry ($id, $name, $translatedName);
+		}
+
+		/** Deletes an ClassUnit from the Table
+		 * @param $id The ID of the ClassUnit to delete
+		 */
+		public function unitDelete ($id) {
+			$this->delEntry ($id);
+		}
+		////////////////////////////////////////////////////////////////////////////////
+		//Implementations
+		////////////////////////////////////////////////////////////////////////////////
+
+}
+
+?>
