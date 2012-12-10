868df69c6b60bfb40282f3031df601a14a305866
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2012-12-10 02:27:24 +0100

    added comments to functions and class

--- code/administrator/headmod_System/modules/mod_User/UsernameAutoCreate.php
+++ code/administrator/headmod_System/modules/mod_User/UsernameAutoCreate.php
@@ -3,20 +3,32 @@
 require_once PATH_INCLUDE . '/constants.php';
 require_once 'UsernameScheme.php';
 
+/**
+ * Allows for automatic creation of Usernames based on predefined rules
+ */
 class UsernameAutoCreate {
 	////////////////////////////////////////////////////////////////////////////////
 	//Constructor
 	////////////////////////////////////////////////////////////////////////////////
 	public function __construct () {
-
 	}
 	////////////////////////////////////////////////////////////////////////////////
 	//Getters and Setters
 	////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * Sets the users so that they can get usernames later on
+	 * @param $users An Array of an Array, containing the database-Values of the
+	 *	Users
+	 */
 	public function usersSet ($users) {
 		$this->users = $users;
 	}
 
+	/**
+	 * Sets the scheme of the usernames,
+	 * @see UsernameScheme
+	 * @param $scheme A instance of UsernameScheme
+	 */
 	public function schemeSet ($scheme) {
 		$this->scheme = $scheme;
 	}
@@ -25,7 +37,9 @@ class UsernameAutoCreate {
 	//Methods
 	////////////////////////////////////////////////////////////////////////////////
 	/**
-	 *
+	 * Creates Usernames for all the Users which were set beforehands with
+	 * UsernameAutoCreat::usersSet. Also it needs a scheme to create the username,
+	 * so make sure you called UsernameAutoCreate::schemeSet ()
 	 */
 	public function usernameCreateAll () {
 		$result = array ();
@@ -39,9 +53,7 @@ class UsernameAutoCreate {
 	////////////////////////////////////////////////////////////////////////////////
 	//Implementations
 	////////////////////////////////////////////////////////////////////////////////
-	protected function usernameCreateByScheme () {
 
-	}
 	////////////////////////////////////////////////////////////////////////////////
 	//Attributes
 	////////////////////////////////////////////////////////////////////////////////
