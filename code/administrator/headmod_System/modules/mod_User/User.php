55cf566ff4e34b991bb9dc0484ea650a7b3319bc
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2012-12-10 02:39:50 +0100

    refactoring of User.php

--- code/administrator/headmod_System/modules/mod_User/User.php
+++ code/administrator/headmod_System/modules/mod_User/User.php
@@ -1,16 +1,11 @@
 <?php
 
-require_once PATH_INCLUDE . '/Module.php';
+require_once 'AdminUserInterface.php';
+require_once 'AdminUserProcessing.php';
 require_once 'UsernameAutoCreate.php';
-
-function test () {
-	$scheme = new UsernameScheme ();
-	$scheme->stringAdd ('TEST');
-	$scheme->templateAdd (UsernameScheme::Forename);
-	$scheme->stringAdd ('blubb');
-	var_dump($scheme->scheme);
-}
-
+require_once PATH_ACCESS . '/CardManager.php';
+require_once PATH_ACCESS . '/UserManager.php';
+require_once PATH_INCLUDE . '/Module.php';
 
 class User extends Module {
 
@@ -26,104 +21,55 @@ class User extends Module {
 	////////////////////////////////////////////////////////////////////////////////
 	//Methods
 	public function execute($dataContainer) {
-
-		defined('_AEXEC') or die('Access denied');
-
-		require_once 'AdminUserInterface.php';
-		require_once 'AdminUserProcessing.php';
-
-		require_once PATH_ACCESS . '/CardManager.php';
-		require_once PATH_ACCESS . '/UserManager.php';
-
-
-
-		test ();
-
-
-		$cm = new CardManager();
-		$um = new UserManager();
-
-		$this->messages = array(
-				'error' => array('no_id' => 'ID nicht gefunden.'));
-
-		$userInterface = new AdminUserInterface($this->relPath);
-		$userProcessing = new AdminUserProcessing($userInterface);
-
+		$this->entryPoint ();
 		if ('POST' == $_SERVER['REQUEST_METHOD']) {
 			$action = $_GET['action'];
 			switch ($action) {
 				case 1: //register user
-					if (isset($_POST['forename'], $_POST['name'], $_POST['username'])) {
-						//form is filled out, register the user
-						try {
-							$userProcessing->RegisterUser($_POST['forename'], $_POST['name'], $_POST['username'], $_POST[
-									'passwd'], $_POST['passwd_repeat'], $_SESSION['CARD_ID'], $_POST["Date_Year"] . '-' . $_POST[
-									"Date_Month"] . '-' . $_POST["Date_Day"], $_POST["gid"], $_POST["credits"], $_POST["class"]);
-						} catch (Exception $e) {
-							$userInterface->dieError($e->getMessage());
-						}
-					} else if (isset($_POST['id'])) {
-						//id is already filled out, show register-form
-						$ar_groups = $userProcessing->getGroups();
-						$_SESSION['CARD_ID'] = $_POST['id'];
-						$userInterface->ShowRegisterForm($ar_groups['arr_gid'], $ar_groups['arr_group_name']);
-					} else {
-						//show card-id-form
-						$userInterface->ShowCardidInput();
-					}
+					$this->userRegister ();
 					break;
 				case 2: //show the users
-					if (isset($_POST['filter'])) {
-						$userProcessing->ShowUsers($_POST['filter']);
-					} else {
-						$userProcessing->ShowUsers("name");
-					}
+					$this->usersShow ();
 					break;
 				case 3: //delete the user
-					if (isset($_POST['delete'])) {
-						$userProcessing->DeleteUser($_GET['ID']);
-					} else if (isset($_POST['not_delete'])) {
-						$userInterface->ShowSelectionFunctionality();
-					} else {
-						$userProcessing->DeleteConfirmation($_GET['ID']);
-					}
+					$this->userDelete ();
 					break;
 				case 4:
 					if (isset ($_POST['user_search'])) {
 						try {
-							$userID = $cm->getUserID($_POST['user_search']);
+							$userID = $this->cardManager->getUserID($_POST['user_search']);
 						} catch (Exception $e) {
 							 $userID =  $e->getMessage();
 						}
 						if ($userID == 'MySQL returned no data!') {
 						try {
-							$userID = $um->getUserID($_POST['user_search']);
+							$userID = $this->userManager->getUserID($_POST['user_search']);
 						} catch (Exception $e) {
-							$userInterface->dieError("Benutzer nicht gefunden!");
+							$this->userInterface->dieError("Benutzer nicht gefunden!");
 						}
 
 					}
 
-						$userProcessing->ChangeUserForm($userID);
+						$this->userProcessing->ChangeUserForm($userID);
 
 						break;
 					}
 
 					if (!isset($_POST['id'], $_POST['forename'], $_POST['name'], $_POST['username'], $_POST['credits'], $_POST[
 					'gid'])) {
-					$userProcessing->ChangeUserForm($_GET['ID']);
+					$this->userProcessing->ChangeUserForm($_GET['ID']);
 					} else {
 						$soli = 0;
 						if (isset($_POST['soliAccount'])) {
 							$soli = 1;
 						}
 						if (isset($_POST['lockAccount'])) {
-							$userProcessing->ChangeUser($_GET['ID'], $_POST['id'], $_POST['forename'], $_POST['name'], $_POST[
+							$this->userProcessing->ChangeUser($_GET['ID'], $_POST['id'], $_POST['forename'], $_POST['name'], $_POST[
 									'username'], $_POST['passwd'], $_POST['passwd_repeat'], $_POST['Date_Year'] . '-' . $_POST[
 									'Date_Month'] . '-' . $_POST['Date_Day'], $_POST['gid'], $_POST['credits'], 1, @$_POST[
 									'cardnumber'], $soli,$_POST['class']);
 						} else {
-							$userProcessing->ChangeUser($_GET['ID'], $_POST['id'], $_POST['forename'], $_POST['name'], $_POST[
+							$this->userProcessing->ChangeUser($_GET['ID'], $_POST['id'], $_POST['forename'], $_POST['name'], $_POST[
 									'username'], $_POST['passwd'], $_POST['passwd_repeat'], $_POST['Date_Year'] . '-' . $_POST[
 									'Date_Month'] . '-' . $_POST['Date_Day'], $_POST['gid'], $_POST['credits'], 0, @$_POST[
 									'cardnumber'], $soli,$_POST['class']);
@@ -136,16 +82,95 @@ class User extends Module {
 			switch ($action) {
 				case 2: //show the users
 					if (isset($_GET['filter'])) {
-						$userProcessing->ShowUsers($_GET['filter']);
+						$this->userProcessing->ShowUsers($_GET['filter']);
 					} else {
-						$userProcessing->ShowUsers("name");
+						$this->userProcessing->ShowUsers("name");
 					}
 			}
 		}
 		else {
-			$userInterface->ShowSelectionFunctionality();
+			$this->userInterface->ShowSelectionFunctionality();
 		}
 	}
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Constructor
+	////////////////////////////////////////////////////////////////////////////////
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Getters and Setters
+	////////////////////////////////////////////////////////////////////////////////
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Methods
+	////////////////////////////////////////////////////////////////////////////////
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Implementations
+	////////////////////////////////////////////////////////////////////////////////
+	protected function entryPoint () {
+		defined('_AEXEC') or die('Access denied');
+		$this->cardManager = new CardManager();
+		$this->userManager = new UserManager();
+		$this->userInterface = new AdminUserInterface($this->relPath);
+		$this->userProcessing = new AdminUserProcessing($this->userInterface);
+		$this->messages = array(
+				'error' => array('no_id' => 'ID nicht gefunden.'));
+	}
+
+	protected function userRegister () {
+		if (isset($_POST['forename'], $_POST['name'], $_POST['username'])) {
+			//form is filled out, register the user
+			try {
+				$this->userProcessing->RegisterUser($_POST['forename'],
+					$_POST['name'], $_POST['username'], $_POST['passwd'],
+					$_POST['passwd_repeat'], $_SESSION['CARD_ID'],
+					$_POST["Date_Year"] . '-' . $_POST["Date_Month"] . '-' .
+					$_POST["Date_Day"], $_POST["gid"], $_POST["credits"],
+					$_POST["class"]);
+			} catch (Exception $e) {
+				$this->userInterface->dieError($e->getMessage());
+			}
+		} else if (isset($_POST['id'])) {
+			//id is already filled out, show register-form
+			$ar_groups = $this->userProcessing->getGroups();
+			$_SESSION['CARD_ID'] = $_POST['id'];
+			$this->userInterface->ShowRegisterForm($ar_groups['arr_gid'], $ar_groups['arr_group_name']);
+		} else {
+			//show card-id-form
+			$this->userInterface->ShowCardidInput();
+		}
+	}
+
+	protected function usersShow () {
+		if (isset($_POST['filter'])) {
+			$this->userProcessing->ShowUsers($_POST['filter']);
+		} else {
+			$this->userProcessing->ShowUsers("name");
+		}
+	}
+
+	protected function userDelete () {
+		if (isset($_POST['delete'])) {
+			$this->userProcessing->DeleteUser($_GET['ID']);
+		} else if (isset($_POST['not_delete'])) {
+			$this->userInterface->ShowSelectionFunctionality();
+		} else {
+			$this->userProcessing->DeleteConfirmation($_GET['ID']);
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	//Attributes
+	////////////////////////////////////////////////////////////////////////////////
+
+	protected $cardManager;
+	protected $userManager;
+	protected $userInterface;
+	protected $userProcessing;
+	protected $messages;
+
 }
 
 ?>
