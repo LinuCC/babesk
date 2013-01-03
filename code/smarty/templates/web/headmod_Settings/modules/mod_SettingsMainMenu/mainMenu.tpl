abae6e2083de3466099d3e9ef8df26e564d5bf1f
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2013-01-03 02:30:32 +0100

    Added 2 new modules for the Headmodule web:Settings : SettingsMainMenu and ChangeEmail. Users can now change the Email with a dialog in web.

---
+++ code/smarty/templates/web/headmod_Settings/modules/mod_SettingsMainMenu/mainMenu.tpl
@@ -0,0 +1,12 @@
+{include file='web/header.tpl' title='Email verändern'}
+
+<div align="center"><h3>Einstellungsmenü</h3></div>
+
+<p>Hier kannst du deine Einstellungen verändern</p>
+<p>Was willst du verändern?</p>
+
+<form action="index.php?section=Settings|ChangeEmail" method="post">
+	<input type="submit" value="Die Email-adresse verändern">
+</form>
+
+{include file='web/footer.tpl'}
---
+++ code/web/headmod_Settings/modules/mod_ChangeEmail/ChangeEmail.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once PATH_INCLUDE . '/Module.php';
+require_once PATH_WEB . '/WebInterface.php';
+require_once PATH_ACCESS . '/UserManager.php';
+
+class ChangeEmail extends Module {
+	/////////////////////////////////////////////////////////////////////
+	//Constructor
+	/////////////////////////////////////////////////////////////////////
+
+	public function __construct($name, $display_name, $path) {
+		parent::__construct($name, $display_name, $path);
+		$this->_smartyPath = PATH_SMARTY . '/templates/web' . $path;
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//Methods
+	/////////////////////////////////////////////////////////////////////
+
+	public function execute ($dataContainer) {
+		$this->entry ();
+		if (!isset($_GET ['action'])) {
+			$this->menuShow ();
+			return;
+		}
+		switch ($_GET ['action']) {
+			case 'changeEmail':
+				$this->emailCheck ();
+				$this->changeSubmit ();
+				$this->finShow ();
+				break;
+		}
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//Implements
+	/////////////////////////////////////////////////////////////////////
+
+	protected function entry () {
+		defined('_WEXEC') or die("Access denied");
+		self::$uid = $_SESSION ['uid'];
+		global $smarty;
+		$this->_smarty = $smarty;
+		$this->_interface = new WebInterface ($this->_smarty);
+		$this->_userManager = new UserManager ();
+	}
+
+	protected function menuShow () {
+		$emailOld = $this->emailOldFetch ();
+		$this->_smarty->assign ('emailOld', $emailOld);
+		$this->_smarty->display ($this->_smartyPath . 'changeEmailDialog.tpl');
+	}
+
+	protected function emailCheck () {
+		try {
+			if (!isset ($_POST ['emailNew'])) {
+				throw new WrongInputException ();
+			}
+			$emailNew = $_POST ['emailNew'];
+			inputcheck ($emailNew, 'email', 'EmailAdresse');
+		} catch (WrongInputException $e) {
+			$this->_interface->DieError (sprintf('Die EmailAdresse "%s" wurde falsch eingegeben.%s', $emailNew, Kuwasys::$buttonBackToMM));
+		}
+	}
+
+	protected function changeSubmit () {
+		$this->_userManager->changeEmailAdress (self::$uid, $_POST ['emailNew']);
+	}
+
+	protected function finShow () {
+		$this->_interface->DieMessage (sprintf ('Die Emailadresse wurde erfolgreich zu "%s" verändert. %s', $_POST ['emailNew'], Kuwasys::$buttonBackToMM));
+	}
+
+	protected function emailOldFetch () {
+		$user = $this->_userManager->getUser (self::$uid);
+		return $user ['email'];
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//Attributes
+	/////////////////////////////////////////////////////////////////////
+
+	protected $_smarty;
+	protected $_interface;
+	protected $_userManager;
+	protected static $uid;
+}
+
+?>
