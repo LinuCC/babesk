a15b5ab42fb32fe4f456677348ae6496fd5883db
Pascal Ernst <pascal.cc.ernst@googlemail.com>
2013-01-05 10:16:33 +0100

    Added Module SettingsChangePassword in web:headmod_settings. It allows the user to change his Password.

--- code/smarty/templates/web/headmod_Settings/modules/mod_SettingsMainMenu/mainMenu.tpl
+++ code/smarty/templates/web/headmod_Settings/modules/mod_SettingsMainMenu/mainMenu.tpl
@@ -8,5 +8,8 @@
 <form action="index.php?section=Settings|ChangeEmail" method="post">
 	<input type="submit" value="Die Email-adresse verändern">
 </form>
+<form action="index.php?section=Settings|SettingsChangePassword" method="post">
+	<input type="submit" value="Das Passwort verändern">
+</form>
 
 {include file='web/footer.tpl'}
---
+++ code/web/headmod_Settings/modules/mod_SettingsChangePassword/SettingsChangePassword.php
@@ -0,0 +1,84 @@
+<?php
+
+class SettingsChangePassword extends Module {
+	/////////////////////////////////////////////////////////////////////
+	//Constructor
+	/////////////////////////////////////////////////////////////////////
+	public function __construct($name, $display_name, $path) {
+		parent::__construct($name, $display_name, $path);
+		$this->_smartyPath = PATH_SMARTY . '/templates/web' . $path;
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//Methods
+	/////////////////////////////////////////////////////////////////////
+	public function execute ($dataContainer) {
+		$this->entry ();
+		if (!isset ($_GET ['action'])) {
+			$this->menuShow ();
+			return;
+		}
+		switch ($_GET ['action']) {
+			case 'changePassword':
+				$this->pwChange ($_POST ['newPassword'], $_POST ['newPasswordRepeat']);
+				break;
+		}
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//Implements
+	/////////////////////////////////////////////////////////////////////
+	protected function entry () {
+		defined('_WEXEC') or die("Access denied");
+		self::$_uid = $_SESSION ['uid'];
+		global $smarty;
+		$this->_smarty = $smarty;
+		$this->_interface = new WebInterface ($this->_smarty);
+		$this->_userManager = new UserManager ();
+	}
+
+	protected function menuShow () {
+		$this->_smarty->display ($this->_smartyPath . 'changePasswordDialog.tpl');
+	}
+
+	protected function pwChange ($pwNew, $pwNewRep) {
+		$this->_pwNew = $pwNew;
+		$this->_pwNewRep = $pwNewRep;
+		$this->pwInpCheck ();
+		$this->pwToDb ();
+		$this->_interface->DieMessage ('Das Passwort wurde erfolgreich verändert');
+	}
+
+	protected function pwToDb () {
+		try {
+			$this->_userManager->changePassword (self::$_uid, $this->_pwNew);
+		} catch (Exception $e) {
+			$this->_interface->DieError ('Konnte das Passwort nicht verändern; Ein interner Fehler ist aufgetreten');
+		}
+	}
+
+	protected function pwInpCheck () {
+		if ($this->_pwNew != $this->_pwNewRep) {
+			$this->_interface->DieError ('Das Passwort stimmt nicht mit der Wiederholung überein. Bitte versuche es noch einmal.');
+		}
+		try {
+			inputcheck ($this->_pwNew, 'password', 'Passwort');
+		} catch (WrongInputException $e) {
+			$this->_interface->DieError ('Das Passwort enthält nicht korrekte Zeichen oder ist zu kurz.');
+		}
+	}
+
+
+	/////////////////////////////////////////////////////////////////////
+	//Attributes
+	/////////////////////////////////////////////////////////////////////
+	protected $_pwNew;
+	protected $_pwNewRep;
+
+	protected static $_uid;
+	protected $_smarty;
+	protected $_interface;
+	protected $_userManager;
+}
+
+?>
