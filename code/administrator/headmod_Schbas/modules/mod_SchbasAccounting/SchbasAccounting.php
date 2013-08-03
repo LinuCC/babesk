e9303149b809a0c1137ced9a760228817301043e
Mirek Hancl <mirek.hancl@leg-uelzen.de>
2013-08-03 22:04:50 +0200

    fixed merge bug

--- code/administrator/headmod_Schbas/modules/mod_SchbasAccounting/SchbasAccounting.php
+++ code/administrator/headmod_Schbas/modules/mod_SchbasAccounting/SchbasAccounting.php
@@ -44,7 +44,7 @@ class SchbasAccounting extends Module {
 					}
 				default:
 					die('Wrong action-value given');
-
+						
 					break;
 			}
 		}
@@ -71,16 +71,23 @@ class SchbasAccounting extends Module {
 			$haystack = array('nl','ln','lr','ls');
 
 			$query = sprintf("SELECT COUNT(*) FROM schbas_accounting WHERE `UID`='%s'",$uid);
-			$result=TableMng::query($query);
+			$result=TableMng::query($query,true);
 			if ($result[0]['COUNT(*)']!="0") {
 				die('dupe');
 			}
 			if(is_numeric($uid) && in_array($loanChoice, $haystack,$true)) {
 				try {
-					$query = sprintf("INSERT INTO schbas_accounting (`UID`,`loanChoice`,`payedAmount`) VALUES ('%s','%s','%s')",$uid,$loanChoice,"0.00");
+					
+					$grade = TableMng::query(sprintf("SELECT g.gradeValue FROM jointusersingrade as juig, grade as g WHERE juig.GradeID=g.ID and juig.UserID='%s'",$uid),true);
+					
+					if ($loanChoice=="ln")	$amountToPay = TableMng::query(sprintf("SELECT fee_normal as fee FROM schbas_fee WHERE grade='%s'",$grade[0]['gradeValue']+1),true);
+					if ($loanChoice=="lr")	$amountToPay = TableMng::query(sprintf("SELECT fee_reduced as fee FROM schbas_fee WHERE grade='%s'",$grade[0]['gradeValue']+1),true);
+					if (!isset($amountToPay)) $amountToPay[0]['fee']="0.00";
+					$query = sprintf("INSERT INTO schbas_accounting (`UID`,`loanChoice`,`payedAmount`,`amountToPay`) VALUES ('%s','%s','%s','%s')",$uid,$loanChoice,"0.00",$amountToPay[0]['fee']);
 
 					TableMng::query($query);
 				} catch (Exception $e) {
+					var_dump($e->getMessage());
 				}
 			}
 			else {
@@ -91,28 +98,23 @@ class SchbasAccounting extends Module {
 			die('error');
 		}
 	}
-
+	
 	private function showUsers () {
-
-		$gradeID = TableMng::query(
-			"SELECT gradeId FROM usersInGradesAndSchoolyears
-				WHERE schoolyearId = @activeSchoolyear");
+		$schoolyearDesired = TableMng::query('SELECT ID FROM schoolYear WHERE active = 1', true);
+		$schoolyearID = $schoolyearDesired[0]['ID'];
+		$gradeID = TableMng::query("SELECT GradeID FROM jointGradeInSchoolYear WHERE SchoolYearID = $schoolyearID",true);
 		foreach ($gradeID as $grade){
-			$ID = $grade['gradeId'];
-			$SaveTheCows = TableMng::query("SELECT * FROM Grades WHERE ID = $ID");
+			$ID = $grade['GradeID'];
+			$SaveTheCows = TableMng::query("SELECT * FROM grade WHERE ID = $ID", true);
 			// Cows stands for Code of worst systematic
 			$gradesAll[] = $SaveTheCows[0];
 		}
-		$users = TableMng::query('SELECT u.*, CONCAT(g.gradelevel, "-", g.label) AS gradeLabel FROM users u
-			JOIN usersInGradesAndSchoolyears uigs ON u.ID = uigs.userId
-			JOIN Grades g ON g.ID = uigs.gradeId
-			WHERE uigs.schoolyearId = @activeSchoolyear
-			ORDER BY name ASC');
+		$users = TableMng::query('SELECT * FROM users ORDER BY name ASC', true);
+		$users = $this->addGradeLabelToUsers($users);
 		$users = $this->addPayedAmountToUsers($users);
-
 		if (isset ($_GET['gradeIdDesired'])){
 			for ($i=0; $i<sizeof($gradesAll); $i++){
-				if ($gradesAll[$i]['gradelevel'].'-'.$gradesAll[$i]['label'] == $_GET['gradeIdDesired']){
+				if ($gradesAll[$i]['gradeValue'].'-'.$gradesAll[$i]['label'] == $_GET['gradeIdDesired']){
 					$gradeDesired = $gradesAll[$i]['ID'];
 				}
 			}
@@ -132,20 +134,19 @@ class SchbasAccounting extends Module {
 		}
 		$this->SchbasAccountingInterface->showAllUsers($gradesAll,$gradeDesired,$users);
 	}
-
-	private function addPayedAmountToUsers ($users) {
-
-		$payed = TableMng::query('SELECT * FROM schbas_accounting');
-		$fees = TableMng::query('SELECT * FROM schbas_fee');
-		foreach ($users as & $user) {
-			foreach ($payed as $pay) {
-				if ($pay['UID'] == $user['ID'])  {
-					$user['payedAmount'] = $pay['payedAmount'];
-					$user['loanChoice'] = $pay['loanChoice'];
-					foreach ($fees as $fee) {
-						if (isset($user['gradeLabel']) && $fee['grade']==preg_replace("/[^0-9]/", "", $user['gradeLabel'])+1) {
-							if ($user['loanChoice']=="ln") $user['amountToPay']=$fee['fee_normal'];
-							if ($user['loanChoice']=="lr") $user['amountToPay']=$fee['fee_reduced'];
+	
+	private function addGradeLabelToUsers ($users) {
+	
+		$jointsUsersInGrade = TableMng::query('SELECT * FROM jointUsersInGrade', true);
+		$grades = TableMng::query('SELECT * FROM grade', true);
+		if (isset($users) && count ($users) && isset($jointsUsersInGrade) && count ($jointsUsersInGrade)) {
+			foreach ($users as & $user) {
+				foreach ($jointsUsersInGrade as $joint) {
+					if ($joint['UserID'] == $user['ID']) {
+						foreach ($grades as $grade) {
+							if ($grade['ID'] == $joint['GradeID']) {
+								$user['gradeLabel'] = $grade['gradeValue'] . '-' . $grade['label'];
+							}
 						}
 					}
 				}
@@ -153,8 +154,30 @@ class SchbasAccounting extends Module {
 		}
 		return $users;
 	}
+	
+	private function addPayedAmountToUsers ($users) {
+	
+		$payed = TableMng::query('SELECT * FROM schbas_accounting', true);
+	//	$fees = TableMng::query('SELECT * FROM schbas_fee', true);
+		foreach ($users as & $user) {
+			foreach ($payed as $pay) {
+				if ($pay['UID'] == $user['ID'])  {		
+					$user['payedAmount'] = $pay['payedAmount'];
+					$user['amountToPay'] = $pay['amountToPay'];
+					$user['loanChoice'] = $pay['loanChoice'];
+// 					foreach ($fees as $fee) {
+// 						if (isset($user['gradeLabel']) && $fee['grade']==preg_replace("/[^0-9]/", "", $user['gradeLabel'])+1) {
+// 							if ($user['loanChoice']=="ln") $user['amountToPay']=$fee['fee_normal'];
+// 							if ($user['loanChoice']=="lr") $user['amountToPay']=$fee['fee_reduced'];
+// 						}
+// 					}
+				}
+			}
+		}
+		return $users;
+	}
 
-
+	
 	private function executePayment($UID, $payment){
 		$UID = str_replace("Payment", "", $UID);
 		try {
@@ -163,7 +186,7 @@ class SchbasAccounting extends Module {
 		} catch (Exception $e) {
 			//die("UPDATE schbas_accounting SET 'payedAmount'=$payment WHERE 'UID'=$UID".$e);
 		}
-
+		
 	}
 
 }
