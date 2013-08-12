6b14cac9a0240ac3a913b3e92eac193790db9ece
Pascal Ernst <pascal.cc.ernst@gmail.com>
2013-08-13 00:15:02 +0200

    Added Base for refactoring Classteachers-Module, Deleted the SchoolYear-Module from Kuwasys (You may want to change your ModuleSettings so that there is no broken link)

---
+++ code/administrator/headmod_Kuwasys/modules/mod_Classteachers/Classteachers.php
@@ -0,0 +1,106 @@
+<?php
+
+require_once PATH_INCLUDE . '/Module.php';
+
+/**
+ * Allows the User to change Classteacher-data
+ *
+ * @author Pascal Ernst <pascal.cc.ernst@gmail.com>
+ */
+class Classteachers extends Module {
+
+	/////////////////////////////////////////////////////////////////////
+	//Constructor
+	/////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Constructs the Module
+	 *
+	 * @param string $name         The Name of the Module
+	 * @param string $display_name The Name that should be displayed to the
+	 *                             User
+	 * @param string $path         A relative Path to the Module
+	 */
+	public function __construct ($name, $display_name, $path) {
+
+		parent::__construct($name, $display_name, $path);
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//Methods
+	/////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Executes the Module, does things based on ExecutionRequest
+	 *
+	 * @param  DataContainer $dataContainer contains data needed by the Module
+	 */
+	public function execute($dataContainer) {
+
+		$this->entryPoint($dataContainer);
+
+		if($execReq = $dataContainer->getSubmoduleExecutionRequest()) {
+			$this->submoduleExecute($execReq);
+		}
+		else {
+			$this->mainMenu();
+		}
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//Implements
+	/////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Initializes data needed by the Object
+	 *
+	 * @param  DataContainer $dataContainer Contains data needed by
+	 *                                      Classteacher
+	 */
+	protected function entryPoint($dataContainer) {
+
+		$this->_interface = $dataContainer->getInterface();
+		$this->_acl = $dataContainer->getAcl();
+		$this->_pdo = $dataContainer->getPdo();
+		$this->_smarty = $dataContainer->getSmarty();
+
+		$this->initSmartyVariables();
+	}
+
+	/**
+	 * Displays a MainMenu to the User
+	 *
+	 * Dies displaying the Main Menu
+	 */
+	protected function mainMenu() {
+
+		$this->_smarty->display(
+			$this->_smartyModuleTemplatesPath . 'mainmenu.tpl');
+	}
+
+
+	/////////////////////////////////////////////////////////////////////
+	//Attributes
+	/////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Handy functions to display things to the User
+	 * @var AdminInterface
+	 */
+	protected $_interface;
+
+	/**
+	 * The AccessControlLayer used for getting the Submodules
+	 * @var Acl
+	 */
+	protected $_acl;
+
+	/**
+	 * The Database-Connection
+	 * @var PDO
+	 */
+	protected $_pdo;
+
+}
+
+?>
