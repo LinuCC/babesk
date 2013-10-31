e4f5d9935b9c73ec835651e68e86ff1fb9e7dae2
Pascal Ernst <pascal.cc.ernst@gmail.com>
2013-10-31 18:39:30 +0100

    Added a new Stored Procedure allowing future DBV revisions to add new Modules

---
+++ code/dbv/data/revisions/5/moduleAddNewByPath.sql
@@ -0,0 +1,56 @@
+CREATE PROCEDURE `moduleAddNewByPath` (
+	IN modulename varchar(255),
+	IN directParentName varchar(255) CHARSET utf8,
+	IN directParentPath text
+	)
+BEGIN
+
+	DECLARE idBuffer int(11);
+	DECLARE pathBuffer text;
+	DECLARE resId int(11);
+	DECLARE done int(1) DEFAULT 0;
+
+	DECLARE moduleBuffer CURSOR FOR
+		SELECT ID FROM Modules
+			WHERE name = directParentName;
+
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+	DECLARE EXIT HANDLER FOR SQLSTATE '42000'
+		SELECT CONCAT('Could not find the given parentModule "',
+			directParentName, '" with its path "', directParentPath);
+
+	OPEN moduleBuffer;
+
+	-- Try to fetch the ID of the Parent by its name and Path
+	REPEAT
+		FETCH moduleBuffer INTO idBuffer;
+		IF NOT done THEN
+
+			-- Check if directParentPath is the same as the selected Modules
+			-- Parent Path; This is because Modules are allowed to have the
+			-- same name
+			SELECT GROUP_CONCAT(parent.name SEPARATOR '/') INTO pathBuffer
+				FROM Modules AS node,
+					Modules AS parent
+				WHERE node.lft BETWEEN parent.lft AND parent.rgt
+					AND node.ID = idBuffer
+				ORDER BY node.lft;
+
+			IF pathBuffer LIKE directParentPath THEN
+				SELECT idBuffer INTO resId;
+				SELECT 1 INTO done;
+			END IF;
+
+		END IF;
+	UNTIL done END REPEAT;
+
+	CLOSE moduleBuffer;
+
+	-- Now add the new Module to the Parent
+	IF resId IS NOT NULL THEN
+		CALL moduleAddNew(modulename, resId);
+	ELSE
+		CALL raise_error;
+	END IF;
+
+END
