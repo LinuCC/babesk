039e5156b0e6117e1e6bffa81069d78d20bdf042
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-01-20 15:02:49 +0100

    Adding indexes to modules-table and groups-table

--- code/dbv/data/revisions/1/Groups.sql
+++ code/dbv/data/revisions/1/Groups.sql
@@ -3,5 +3,7 @@ CREATE TABLE `Groups` (
   `name` varchar(255) COLLATE latin1_german2_ci NOT NULL,
   `lft` int(11) NOT NULL,
   `rgt` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  Key `ixLft` (`lft`),
+  Key `ixRgt` (`rgt`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci
--- code/dbv/data/revisions/1/Modules.sql
+++ code/dbv/data/revisions/1/Modules.sql
@@ -6,5 +6,7 @@ CREATE TABLE `Modules` (
   `enabled` tinyint(1) NOT NULL,
   `executablePath` varchar(255) COLLATE latin1_german2_ci NOT NULL,
   `displayInMenu` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `ixLft` (`lft`),
+  KEY `ixRgt` (`rgt`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci
