53bb7a5c56991cd370a7f50e6458a111833f9ca0
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-01-20 14:52:39 +0100

    Remove one Index, not needed (at least for now)

--- code/dbv/data/revisions/14/system_adminBookmarks.sql
+++ code/dbv/data/revisions/14/system_adminBookmarks.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS `adminBookmarks` (
   `bmid` enum('0','1','2','3','4') NOT NULL,
   `mid` int(11) NOT NULL,
   KEY `ixUserId` (`uid`),
-  KEY `ixBmidMid` (`bmid`, `mid`),
   PRIMARY KEY(`uid`, `bmid`, `mid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
