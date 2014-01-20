ec2f852c5aef94e7a46c6fe1478ac91d9e7fc807
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-01-20 14:30:40 +0100

    Added heavy indexing to the adminBookmarks-table sqlfile

--- code/dbv/data/revisions/14/system_adminBookmarks.sql
+++ code/dbv/data/revisions/14/system_adminBookmarks.sql
@@ -1,5 +1,8 @@
 CREATE TABLE IF NOT EXISTS `adminBookmarks` (
   `uid` bigint(11) NOT NULL,
   `bmid` enum('0','1','2','3','4') NOT NULL,
-  `mid` int(11) NOT NULL
+  `mid` int(11) NOT NULL,
+  KEY `ixUserId` (`uid`),
+  KEY `ixBmidMid` (`bmid`, `mid`),
+  PRIMARY KEY(`uid`, `bmid`, `mid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
