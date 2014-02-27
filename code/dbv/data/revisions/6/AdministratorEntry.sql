8a4588cb51910932e50c4a722e5a6ca09fd2372f
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-02-27 19:51:27 +0100

    Fixing a problem with one db-revision not defining values for columns without default-values when adding the administrator.

--- code/dbv/data/revisions/6/AdministratorEntry.sql
+++ code/dbv/data/revisions/6/AdministratorEntry.sql
@@ -1,6 +1,8 @@
-INSERT INTO `users` (ID, forename, name, username, password) VALUES
-	-- Default Password of administrator is root
-	(1, 'admin', 'admin', 'administrator', '63a9f0ea7bb98050796b649e85481845');
+INSERT INTO `users` (ID, forename, name, username, password, email, telephone, birthday, last_login, locked, GID, credit, soli) VALUES (
+		1, 'admin', 'admin', 'administrator',
+		-- Default Password of administrator is root
+		'63a9f0ea7bb98050796b649e85481845',
+		'', '', '', '', 0, 0, 0.00, 0
+	);
 
-INSERT INTO `UserInGroups` (userId, groupId) VALUES
-	(1, 3);
+INSERT INTO `UserInGroups` (userId, groupId) VALUES (1, 3);
