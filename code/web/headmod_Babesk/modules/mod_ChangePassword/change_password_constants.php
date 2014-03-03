386a1cc16abe0034aeadbc5fc5920d49b040691f
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-03-03 21:30:47 +0100

    Removed not used Module web/Babesk/ChangePassword

--- code/web/headmod_Babesk/modules/mod_ChangePassword/change_password_constants.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	define('CH_P_WRONG_PW', '<p class="error">Das Passwort muss mindestens 4 Zeichen lang sein und darf keine Sonderzeichen enthalten</p>');
-	define('CH_P_OLD_PW', '<p class="error">Das Passwort entspricht dem alten Passwort. Bitte ändern sie das Passwort.</p>');
-	
-	define('ERR', '<p class="error">Ein Fehler ist aufgetreten. Konnte den Vorgang nicht ausführen</p>');
-?>
