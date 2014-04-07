30490db4926067fe644c8a76e7927c9bf805db2d
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-04-07 15:16:40 +0200

    added Procedure to delete moduels by path; moved module Kuwasys/Grade to System

---
+++ dbv/data/revisions/24/addProcedure_moduleLeafDeleteByPath.sql
@@ -0,0 +1,48 @@
+CREATE PROCEDURE `moduleLeafDeleteByPath`(
+	IN modulename varchar(255),
+	IN modulepath text
+	)
+BEGIN
+
+	DECLARE idBuffer int(11);
+	DECLARE pathBuffer text;
+	DECLARE done int(1) DEFAULT 0;
+	DECLARE moduleId int(11) DEFAULT 0;
+
+	DECLARE moduleBuffer CURSOR FOR
+		SELECT ID FROM SystemModules
+			WHERE name = modulename;
+
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+	DECLARE EXIT HANDLER FOR SQLSTATE "42000"
+		SELECT CONCAT("Could not find the given module ",
+			modulename, " with its path ", modulepath);
+
+	OPEN moduleBuffer;
+
+	-- Try to fetch the ID of the Parent by its name and Path
+	REPEAT
+		FETCH moduleBuffer INTO idBuffer;
+		IF NOT done THEN
+
+			-- Check if modulepath is the same as the selected Modules
+			-- Path; This is because Modules are allowed to have the
+			-- same name
+			SELECT GROUP_CONCAT(parent.name SEPARATOR '/') INTO pathBuffer
+				FROM SystemModules AS node,
+					SystemModules AS parent
+				WHERE node.lft BETWEEN parent.lft AND parent.rgt
+					AND node.ID = idBuffer
+				ORDER BY node.lft;
+
+			IF pathBuffer LIKE modulepath THEN
+				SELECT idBuffer INTO moduleId;
+				SELECT 1 INTO done;
+			END IF;
+
+		END IF;
+	UNTIL done END REPEAT;
+
+	CALL moduleDelete(moduleId);
+
+END
---
+++ dbv/data/revisions/25/deleteGradeModulesOfKuwasys.sql
@@ -0,0 +1,5 @@
+CALL moduleLeafDeleteByPath('ChangeGrade', 'root/administrator/Kuwasys/Grade/ChangeGrade');
+CALL moduleLeafDeleteByPath('DeleteGrade', 'root/administrator/Kuwasys/Grade/DeleteGrade');
+CALL moduleLeafDeleteByPath('AddGrade', 'root/administrator/Kuwasys/Grade/AddGrade');
+CALL moduleLeafDeleteByPath('ShowGrades', 'root/administrator/Kuwasys/Grade/ShowGrades');
+CALL moduleLeafDeleteByPath('Grade', 'root/administrator/Kuwasys/Grade');
