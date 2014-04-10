4399beb42a6a55e4ccb2821f3ad6327077099413
Pascal Ernst <pascal.cc.ernst@gmail.com>
2014-04-10 07:59:16 +0200

    Added Javscript Microtemplating-"engine"
    Allows to parse html-snippets and replace variables (even allows for embedded
    scripts in the tags). See http://ejohn.org/blog/javascript-micro-templating/
    for more explanation.

--- code/include/js/custom-base.js
+++ code/include/js/custom-base.js
@@ -52,10 +52,53 @@ if(!navigator.cookieEnabled) {
 	toastr['error']('Cookies sind nicht aktiviert! Diese Website benötigt Cookies um zu funktionieren.', 'Cookies');
 }
 
-$('body').tooltip({
-	selector: 'a[rel="tooltip"], [data-toggle="tooltip"]'
-});
+(function refreshTooltips() {
+	$('body').tooltip({
+		selector: 'a[rel="tooltip"], [data-toggle="tooltip"]'
+	});
+})();
+
 
 $('body').popover({
 	selector: 'a[rel="popover"], [data-toggle="popover"]'
 });
+
+
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+// Client side template parser that uses &lt;?= #&gt; and &lt;? code ?&gt;
+// expressions and ? ? code blocks for template expansion.
+// NOTE: chokes on single quotes in the document in some situations
+//       use &amp;rsquo; for literals in text and avoid any single quote
+//       attribute delimiters.
+(function() {
+		var cache = {};
+
+		this.microTmpl = function microTmpl(str, data) {
+				// Figure out if we're getting a template, or if we need to
+				// load the template - and be sure to cache the result.
+				var fn = !/\W/.test(str) ?
+			cache[str] = cache[str] ||
+				microTmpl(document.getElementById(str).innerHTML) :
+
+				// Generate a reusable function that will serve as a template
+				// generator (and which will be cached).
+			new Function("obj",
+				"var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+				// Introduce the data as local variables using with(){}
+				"with(obj){p.push('" +
+
+				// Convert the template into pure JavaScript
+				str.replace(/[\r\t\n]/g, " ")
+					 .replace(/'(?=[^%]*%>)/g,"\t")
+					 .split("'").join("\\'")
+					 .split("\t").join("'")
+					 .replace(/<%=(.+?)%>/g, "',$1,'")
+					 .split("<%").join("');")
+					 .split("%>").join("p.push('")
+					 + "');}return p.join('');");
+				// Provide some basic currying to the user
+				return data ? fn(data) : fn;
+		};
+})();
