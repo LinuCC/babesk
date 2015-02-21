748560760806bae10ef05c96440bca5218abde59
Pascal Ernst <p.ernst@ostfalia.de>
2015-02-21 22:35:12 +0100

    Add default-values for missing entries in SystemGlobalSettings for #45

---
+++ dbv/data/revisions/44/addEntries_SystemGlobalSettings.sql
@@ -0,0 +1,13 @@
+INSERT INTO `global_settings`
+	(name, value) VALUES
+	-- All existing foreign languages of SchoolSubjects
+	('foreign_language', '')
+	-- All existing religions of SchoolSubjects
+	('religion', '')
+	-- All special courses
+	('special_course', '')
+	-- Fits-specific values
+	('fits_key', '')
+	('fits_year', '')
+	('fits_all_classes', '')
+	('fits_class', '');
