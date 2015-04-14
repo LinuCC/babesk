3da46cb3acd881175fd1bc0a620a60aff349ca8b
Pascal Ernst <p.ernst@ostfalia.de>
2015-04-14 02:37:59 +0200

    Auto-hide the admin-sidebar if viewport smaller than 1000 px

--- code/include/js/administrator/main.js
+++ code/include/js/administrator/main.js
@@ -7,25 +7,10 @@ $(document).ready(function() {
 	});
 
 	$('.sidebar-toggle').on('click', function(ev) {
-
 		$('#body-wrapper').toggleClass('show-sidebar');
-		return;
-		var $sidebar = $('.sidebar');
-		var $mainWrapper =
-
-		console.log(($sidebar.css('width')));
-
-		if(parseInt($sidebar.css('width')) > 1) {
-			$sidebar.css('width', '0px');
-			$mainWrapper.css('margin-left', '0px');
-			//$sidebar.animate({'width' : '0px'}, 400);
-			//$mainWrapper.animate({'margin-left': '0'}, 400);
-		}
-		else {
-			$sidebar.css('width', '250px');
-			$mainWrapper.css('margin-left', '250px');
-			//$sidebar.animate({'width' : '250px'}, 400);
-			//$mainWrapper.animate({'margin-left': '250px'}, 400);
-		}
 	});
+
+	if($('body').width() < 1000) {
+		$('#body-wrapper').removeClass('show-sidebar');
+	}
 });
